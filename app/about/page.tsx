842919ddafc15c7179a3de6b077f17d6e0bc32dc
Yi-Jacob <jacobyideveloper@gmail.com>
2024-03-23 20:31:41 -0500

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { Navigation } from "../components/nav";
+
+export default function About() {
+  return (
+    <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+      <Navigation />
+      <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
+        <div className="max-w-2xl mx-auto my-3 lg:mx-0">
+          <h2
+            className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl"
+            style={{ marginTop: "75px" }}
+          >
+            About
+          </h2>
+          <p className="mt-4 text-zinc-400">
+            Hey there! I'm Jacob Yi, a full-stack software developer with a
+            knack for crafting dynamic and user-friendly web and mobile
+            applications. My journey in the tech industry has been fueled by a
+            strong passion for coding and a solid background in computer
+            science. Over the years, I've had the opportunity to work on a
+            variety of projects, from simple websites to complex applications,
+            always striving to deliver top-notch results on time.
+          </p>{" "}
+          <p className="mt-4 text-zinc-400">
+            Beyond the realm of coding, I find immense joy in the simple
+            pleasures of life. My Christian faith is the cornerstone of my
+            existence, providing me with strength, purpose, and a sense of
+            belonging. Family holds an irreplaceable place in my heart, serving
+            as my foundation and constant motivation. And when it comes to
+            freedom, it's not just about financial independence but also about
+            embracing life's adventures, whether it's through travel or the
+            invigorating rhythm of running.
+          </p>
+          <p className="mt-4 text-zinc-400">
+            Speaking of travel, it's one of my greatest passions. There's
+            something magical about immersing myself in different cultures,
+            exploring new landscapes, and meeting people from all walks of life.
+            Each journey leaves me with cherished memories and a deeper
+            appreciation for the world around me.
+          </p>
+          <p className="mt-4 text-zinc-400">
+            On the flip side, I also find solace in the simplicity of lacing up
+            my running shoes and hitting the pavement. Running isn't just about
+            physical fitness for me; it's a form of meditation, a way to clear
+            my mind and reconnect with myself. Whether it's a leisurely jog
+            through the park or a challenging race, the feeling of freedom that
+            comes with each step is truly exhilarating.
+          </p>
+          <p className="mt-4 text-zinc-400">
+            Looking ahead, I'm excited about the endless possibilities that lie
+            on the horizon. Whether it's diving deeper into the world of web
+            development, embarking on new travel adventures, or simply spending
+            quality time with loved ones, I'm eager to embrace each opportunity
+            with open arms. Life is a beautiful journey, and I'm grateful for
+            every moment of it. Who knows where I'll be next? Perhaps leading as
+            the CEO of a successful startup, pounding the pavement as a
+            dedicated marathon runner, earning recognition as a Forbes 30 under
+            30 honoree, or backpacking across the globe, soaking in the diverse
+            cultures and breathtaking landscapes. Whatever the future holds, I'm
+            ready to embrace the challenges and celebrate the victories along
+            the way.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
+  { name: "About", href: "/about" },
   { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
 ];
@@ -29,11 +30,11 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        Jacob Yi
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
+      {/* <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
           I'm building{" "}
           <Link
@@ -44,7 +45,7 @@ export default function Home() {
             unkey.dev
           </Link> to solve API authentication and authorization for developers.
         </h2>
-      </div>
+      </div> */}
     </div>
   );
 

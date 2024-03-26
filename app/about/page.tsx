15b89f41b7436145a0aaa0175dcffde405a5380f
Yi-Jacob <jacobyideveloper@gmail.com>
2024-03-26 10:17:03 -0500

    patch

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -48,32 +48,32 @@ export default function About() {
             landscapes. Whatever the future holds, I'm ready to embrace the
             challenges and celebrate the victories along the way.
           </p>
-          <div className="flex">
-          <div className="pb-4 z-10 flex flex-col items-center">
-                <a
-                  href={`/blog/introduction`}
-                  className="text-white border border-white rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none hover:bg-white hover:text-black"
-                >
-                  Introduction
-                </a>
-              </div>
-              <div className="pb-4 z-10 flex flex-col items-center">
-                <a
-                  href={`/blog/jacobs-identities`}
-                  className="text-white border border-white rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none hover:bg-white hover:text-black"
-                >
-                  Frameworks of My Identity
-                </a>
-              </div>
-              <div className="pb-4 z-10 flex flex-col items-center">
-                <a
-                  href={`/blog/jacobs-hobbies`}
-                  className="text-white border border-white rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none hover:bg-white hover:text-black"
-                >
-                  Discovering My World
-                </a>
-              </div>
+          <div className="flex mt-3">
+            <div className="pb-4 z-10 flex flex-col items-center">
+              <a
+                href={`/blog/introduction`}
+                className="text-white border border-white rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none hover:bg-white hover:text-black"
+              >
+                Introduction
+              </a>
             </div>
+            <div className="pb-4 z-10 flex flex-col items-center">
+              <a
+                href={`/blog/jacobs-identities`}
+                className="text-white border border-white rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none hover:bg-white hover:text-black"
+              >
+                Frameworks of My Identity
+              </a>
+            </div>
+            <div className="pb-4 z-10 flex flex-col items-center">
+              <a
+                href={`/blog/jacobs-hobbies`}
+                className="text-white border border-white rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none hover:bg-white hover:text-black"
+              >
+                Discovering My World
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
